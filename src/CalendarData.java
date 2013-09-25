64b6c44781d7331dd29ae205c06f649ba4ba6156
John Clevenger V <jcleveng@c15-0330-13.ad.mtu.edu>
2013-09-25 14:59:13 -0400

    Added Event Saving and Loading

--- src/CalendarData.java
+++ src/CalendarData.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class CalendarData {
@@ -35,14 +31,20 @@ public class CalendarData {
 	}
 	public void Add(Event e)
 	{
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		String query = String.format("insert into events (`name`,`location`,`desc`,`month`,`day`,`year`,`hour`,`minute`,`duration`) values(%s, %s, %s, %d, %d, %d, %d, %d, %f);", e.GetName(), e.GetLocation(), e.GetDesc(), e.GetMonth(), e.GetDay(), e.GetYear(), e.GetTimeHour(), e.GetTimeMinute(), e.GetDuration());
+		String insertSQL = "insert into events (`name`,`location`,`desc`,`month`,`day`,`year`,`hour`,`minute`,`duration`) values(?, ?, ?, ?, ?, ?, ?, ?, ?);";
 		try
 		{
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(query);
+			PreparedStatement preparedStatement = con.prepareStatement(insertSQL);
+			preparedStatement.setString(1, e.GetName());
+			preparedStatement.setString(2, e.GetLocation());
+			preparedStatement.setString(3, e.GetDesc());
+			preparedStatement.setInt(4, e.GetMonth());
+			preparedStatement.setInt(5, e.GetDay());
+			preparedStatement.setInt(6, e.GetYear());
+			preparedStatement.setInt(7, e.GetTimeHour());
+			preparedStatement.setInt(8, e.GetTimeMinute());
+			preparedStatement.setFloat(9, e.GetDuration());
+			preparedStatement.executeUpdate();
 		}
 		catch(SQLException ex)
 		{
@@ -50,6 +52,70 @@ public class CalendarData {
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());
 		}
-
+	}
+	
+	public int GetCount(int day, int month, int year)
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+		String selectSQL = String.format("select * from events where day = %d and month = %d and year = %d;", day, month, year);
+		try
+		{
+			stmt = con.createStatement();
+			
+			rs = stmt.executeQuery(selectSQL);
+			
+			rs.last();
+			return rs.getRow();
+		}
+		catch(SQLException ex)
+		{
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return 0;
+	}
+	
+	public Event[] GetEvents(int day, int month, int year)
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+		String selectSQL = String.format("select * from events where day = %d and month = %d and year = %d;", day, month, year);
+		try
+		{
+			stmt = con.createStatement();
+			
+			rs = stmt.executeQuery(selectSQL);
+			
+			rs.last();
+			int count = rs.getRow();
+			rs.beforeFirst();
+			Event[] e = new Event[count];
+			int i = 0;
+			while(rs.next())
+			{
+				e[i] = new Event();
+				e[i].SetName(rs.getString(2));
+				e[i].SetLocation(rs.getString(3));
+				e[i].SetDesc(rs.getString(4));
+				e[i].SetDay(rs.getInt(5));
+				e[i].SetMonth(rs.getInt(6));
+				e[i].SetYear(rs.getInt(7));
+				e[i].SetTimeHour(rs.getInt(8));
+				e[i].SetTimeMin(rs.getInt(9));
+				e[i].SetDuration(rs.getFloat(10));
+				i++;
+			}
+			return e;
+		}
+		catch(SQLException ex)
+		{
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+		return null;
 	}
 }
+
